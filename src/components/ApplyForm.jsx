b38cd553b2a53ac1c548f5f764b4437b79dba0b2
muhammadkashif20 <kashif28@gmail.com>
2024-11-08 23:56:13 +0500

    Create Form Schema with Zod & Using shadcn Components Working On Display Form in Frontend

--- src/components/ApplyForm.jsx
+++ src/components/ApplyForm.jsx
@@ -6,6 +6,28 @@ const formSchema = z.object({
   name: z.string().min(2).max(50),
   bio: z.string().min(2).max(110),
   hospital: z.string().min(2).max(50),
-  
-  
+  fees: z.string().min(2).max(50),
+  degree: z.string().min(2).max(50),
+  degree: z.string().min(2).max(50),
+  degree: z.string(),
+  gender: z.string(),
+  experiance: z.string(),
+  specialization: z.string(),
+  appointmentTime: z.date(),
+  profileImage:z.string(),
+  contact:z.string(),
+  email:z.string(),
+  address:z.string(),
 })
+export function ProfileForm() {
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+}
+
+function onSubmit(values) { 
+    console.log(values)
+  }
